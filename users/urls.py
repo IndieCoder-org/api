7722d2cc68f3fe4e4e408a2a5a301ef9c66c14ac
osamamhd <osamamhd82@gmail.com>
2021-07-31 18:42:17 +0200

    ✨ add path for user/register, user/detail and user update.

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
+
+urlpatterns = [
+    path('users/register', views.user_create),
+    path('user/', views.user_details),
+    path('users/<int:pk>', views.user_update),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
