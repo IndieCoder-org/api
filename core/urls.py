60d09b3adc840556721d931e8f02d1c6bb1ee221
osamamhd <osamamhd82@gmail.com>
2021-08-10 04:01:47 +0200

    🔨 [core] add posts app to local apps and posts.urls to core urls.

--- core/urls.py
+++ core/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('users.urls')),
+    path('posts/', include('posts.urls')),
 ]
