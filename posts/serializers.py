c03d731af9ae3fa514a28e617299900fef5526d1
osamamhd <osamamhd82@gmail.com>
2021-08-12 19:50:16 +0200

    🌱 Post serializer for the post model.

---
+++ posts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.StringRelatedField()
+
+    def validate_date(self, value):
+        if self.instance and self.instance.date != value:
+            raise ValidationError("You may not edit date!")
+        return value
+
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'title',
+            'content',
+            'up_votes',
+            'owner',
+            'format_created_at',
+            'format_updated_at',
+            'get_absolute_url',
+            'image',
+            'get_image',
+            'get_thumbnail',
+        ]
+        read_only_fields = ['owner', 'up_votes']
