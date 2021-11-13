96f28760734fcd7afe4ddee635f3789225ce07d1
Umair Karel <umair.karel@gmail.com>
2021-11-13 12:24:25 +0530

    Added Meet-Up model

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class MeetUp(models.Model):
+    title = models.CharField(max_length=30)
+    where = models.CharField(max_length=20)
+    when = models.DateTimeField()
+    description = models.TextField()
+    url = models.URLField()
